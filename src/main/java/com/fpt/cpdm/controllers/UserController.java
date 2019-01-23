cd287022eefd4a03d01460192c23aeeed808013e
nightlonely7 <Speedup79>
2019-01-23 13:03:56 +0700

    [fix]

--- src/main/java/com/fpt/cpdm/controllers/UserController.java
+++ src/main/java/com/fpt/cpdm/controllers/UserController.java
@@ -8,7 +8,6 @@ import com.fpt.cpdm.utils.ModelErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
