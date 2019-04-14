285dcb5c92696d522185196c92ac9bdc8790375a
MrQuang <QuangHVSE61073@fpt.edu.vn>
2019-04-14 18:11:14 +0700

    [fix]

--- src/main/java/com/fpt/cpdm/models/tasks/TaskDetail.java
+++ src/main/java/com/fpt/cpdm/models/tasks/TaskDetail.java
@@ -1,10 +1,13 @@
 package com.fpt.cpdm.models.tasks;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public interface TaskDetail extends TaskSummary {
 
     String getDescription();
 
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     LocalDateTime getLastModifiedTime();
 }
