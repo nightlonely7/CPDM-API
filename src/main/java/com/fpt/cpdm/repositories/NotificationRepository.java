a7995ad4491d37437460aa4f09921a78df473339
NhanNHB <NhanNHB92@gmail.com>
2019-05-06 13:30:09 +0700

    [Notification] Add auto hide and delete old notification

--- src/main/java/com/fpt/cpdm/repositories/NotificationRepository.java
+++ src/main/java/com/fpt/cpdm/repositories/NotificationRepository.java
@@ -5,8 +5,11 @@ import com.fpt.cpdm.entities.UserEntity;
 import com.fpt.cpdm.models.notifications.NotificationSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<NotificationEntity,Integer> {
     List<NotificationSummary> findAllByUserAndHiddenOrderByCreatedTimeDesc(UserEntity userEntity, boolean hidden);
+
+    List<NotificationEntity> findAllByHiddenFalseAndCreatedTimeIsLessThanEqual(LocalDateTime time);
 }
